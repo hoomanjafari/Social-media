2fa94d7becd5b6529072979a5010bc24384cc3a3
hooman <hooman.jafari3647@yahoo.com>
2024-01-14 02:07:26 +0330

    added static directory and base.html background edited

--- SocialMediaII/settings.py
+++ SocialMediaII/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6_o+5735n-wp(1ev7mj$hdl0ocsc(nhb43#s38#eidukv)tp&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
